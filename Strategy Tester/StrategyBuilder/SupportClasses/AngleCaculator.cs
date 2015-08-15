2eba1d9cd1f67b5ead34f622f0a490838179f5fb
kentmkmb <kent.mkmb@gmail.com>
2015-08-15 17:42:36 +0500

    Исправил вычисление угла в AngleCaculator

--- Strategy Tester/StrategyBuilder/SupportClasses/AngleCaculator.cs
+++ Strategy Tester/StrategyBuilder/SupportClasses/AngleCaculator.cs
@@ -20,13 +20,13 @@ namespace StrategyBuilder
             var horizontal = new Vector(new PointD(0, 0), new PointD(1, 0));
             var current = new Vector(begin, end);
             if (current.X == 0)
-                return current.Y > 0 ? Math.PI/2 : 3*Math.PI/2;
+                return current.Y > 0 ? 3 * Math.PI / 2 : Math.PI / 2;
             if (current.Y == 0)
                 return current.X > 0 ? 0 : Math.PI;
             var scalarMult = (horizontal.X * current.X) + (horizontal.Y * current.Y);
             var angleCos = scalarMult / (horizontal.Length * current.Length);
             var angle = Math.Acos(angleCos);
-            return current.Y > 0 ? angle : 2*Math.PI - angle;
+            return current.Y < 0 ? angle : 2*Math.PI - angle;
         }
     }
 }
